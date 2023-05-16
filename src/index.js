3f77f77da580d586f2c911b2b65109f81fbc618f
PaganJorm <scarletemperor@protonmail.com>
2023-05-16 23:52:44 +0200

    aaaaa

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import NoPage from "./components/noPage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="search" element={<Search />} />
